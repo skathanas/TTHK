00748e19caeea64a5d381e5f358de401750e4346
Skathanas <Jarmo@Jarmos-MacBook-Pro.local>
2019-09-06 17:03:08 +0300

    My First Commit

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace API
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name");
+            string name;
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Sinu nimi on " + name);
+            Console.ReadLine();
+
+        }
+    }
+}
